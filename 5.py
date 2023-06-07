76990486bba231d14bc67326ef8aa64727af5c13
akashoffical1473 <135795607+akashoffical1473@users.noreply.github.com>
2023-06-07 06:35:32 +0000

    base and power

---
+++ 5.py
@@ -0,0 +1,7 @@
+#Write a Python function that takes base and power as arguments and calculates the power of numbers. 
+def power(b,p):
+    po=(b**p)
+    print(po)
+b=int(input("Enter base number: "))
+p=int(input("Enter power: "))
+power(b,p)
