2a297cea9e51421f722c2f05c0fc38f58e4b7afa
akashoffical1473 <135795607+akashoffical1473@users.noreply.github.com>
2023-06-07 06:29:09 +0000

    sqr using functions

---
+++ 2.py
@@ -0,0 +1,14 @@
+#Perfect square or not using functions
+
+n=int(input("Enter the number: "))
+
+def num(n):
+    i=1
+    while i<=n:
+        if n==i*i:
+            return print("Perfect square number")
+        i=i+1
+    else:
+        return print("not a perfect square number")
+num(n)
+
