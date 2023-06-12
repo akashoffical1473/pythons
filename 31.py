8c401648a909a36f1abc00e37936d375c685dec7
akashoffical1473 <135795607+akashoffical1473@users.noreply.github.com>
2023-06-12 09:12:30 +0000

    separated by a spaces and swap

---
+++ 31.py
@@ -0,0 +1,14 @@
+'''Write a Python program to get a single string from two given strings, separated by a space and swap the first two characters of each string.
+Sample Input
+Mike
+Dice
+Sample Output
+Dike
+Mice
+'''
+st1=input("Enter the 1st string: ")
+st2=input("Enter the 2nd string: ")
+st=st1+" "+st2
+print(st)
+x=st2[0:2]+st1[2:]+" "+st1[0:2]+st2[2:]
+print(x)
